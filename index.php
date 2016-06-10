c1989c44c024a7053931937b35724632fdc889b3
BISCARO FABIO <biscarofabio@ITSEINAUDI.LOCAL>
2016-06-10 13:15:53 +0200

    prova

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-ciao
+<?php
+$servername = "lab06_cattedra1";
+$username = "peertopeer";
+$password = "peertopeer";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
